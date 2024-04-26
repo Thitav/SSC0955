854978094e79533015e5147c142bcc35522f972f
Thitav <thiptav@gmail.com>
2024-04-26 02:33:07 -0300

    TTY

--- tty.s
+++ tty.s
@@ -1,130 +1,94 @@
-jmp vars_e
-vars:
-  _ : var #1200
-  ebuf : 
+data:
+  buffer : var #1200
+  buffer_end :
+data_e:
 
-  tstruct:
-  ts_reader : var #1
-  ts_writer : var #1
-  ts_eaddr : var #1
-  ts_bsize : var #1
+; r1 reader
+; r2 writer
+; r3 buffer
+; r4 buffer_end
 
-  static ts_reader, #1200
-  static ts_writer, #1200
-  static ts_eaddr, #ebuf
-  static ts_bsize, #1200
-vars_e:
+loadn r1, #buffer
+mov r2, r1
+mov r3, r1
+loadn r4, #buffer_end
 
-loadn r0, #0
-loadn r1, #tstruct
+loadn r5, #0
+loadn r6, #1200
 main:
   call getc
-  ; outchar r7, r0
-  mov r2, r7
+  outchar r7, r5
+
+  inc r5
+  cmp r5, r6
+  jne main_ne
+  push r4
+  loadn r4, #40
+  sub r5, r5, r4
+  pop r4
+
+  main_ne:
+  mov r0, r7
   call twrite
 
-  inc r0
   jmp main
 
 halt
 
-; in r0 : cursor
-; in * r1 : tstruct
 ; out r7 : caractere
 tread:
-  push r2
-  push r3
-  push r1
-
-  loadi r2, r1 ; reader
+  loadi r7, r1
   inc r1
-  inc r1
-  loadi r3, r1 ; eaddr
 
-  sub r3, r3, r2 
-  loadi r7, r3
-
-  dec r2
-  jnz tread_rts
-  inc r1
-  loadi r2, r1 ; reader = size
+  cmp r1, r4
+  jne tread_rts
+  mov r1, r3
 
   tread_rts:
-    pop r1
-    storei r1, r2
-
-    pop r3
-    pop r2
     rts
 
-; in * r1 : tstruct
-; in r2 : caractere
+; in r0 : caractere
 twrite:
-  ; breakp
-  push r3
-  push r4
-  push r1
+  storei r2, r0
+  inc r2
 
-  outchar r2, r0
+  cmp r2, r4
+  jne twrite_ne
+  mov r2, r3
 
-  inc r1
-  loadi r3, r1 ; writer
-  inc r1
-  loadi r4, r1 ; eaddr
-
-  sub r4, r4, r3
-  storei r4, r2
-
-  dec r3
-  jnz twrite_nz
-
-  inc r1
-  loadi r3, r1 ; writer = size
-
-  twrite_nz:
-    pop r1
-    loadi r5, r1 ; reader
-    push r1
-    inc r1 ; writer
-    cmp r3, r5
-    jne twrite_rts
-
-    loadn r6, #40
-    sub r5, r5, r6
-    dec r1
-    storei r1, r5
-    inc r1 
-    add r4, r4, r6
-
-    push r1
-    push r2
-    push r3
-
-    mov r1, r4
-    loadn r2, #0
-    mov r3, r6
-    call memset
-
-    pop r3
-    pop r2
-    pop r1
-
-    loadn r6, #0
-    loadn r5, #1200
-    twrite_nz_loop:
-      call tread
-      outchar r7, r6
-
-      dec r4
-      inc r6
-      cmp r6, r5
-      jne twrite_nz_loop
+  twrite_ne:
+  cmp r2, r1
+  jne twrite_rts
+  call tflush
 
   twrite_rts:
-    ; pop r1
-    storei r1, r3
+    rts
 
-    pop r1
-    pop r4
-    pop r3
+tflush:
+  push r5
+  push r6
+
+  push r2
+  push r3
+  loadn r2, #0
+  loadn r3, #40
+  call memset
+  add r1, r1, r3
+  sub r2, r2, r3
+  pop r3
+  pop r2
+
+  loadn r5, #0
+  loadn r6, #1200
+  tflush_loop:
+    call tread
+    outchar r7, r5
+
+    inc r5
+    cmp r5, r6
+    jne tflush_loop
+
+  tflush_rts:
+    pop r6
+    pop r5
     rts
